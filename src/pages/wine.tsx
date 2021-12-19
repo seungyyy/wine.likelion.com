a084c2118adde3248c9b16757c4a4b540cb5155a
seungyeon-lee3214 <54584337+seungyeon-lee3214@users.noreply.github.com>
2021-12-19 20:55:17 +0900

    create wine page

---
+++ src/pages/wine.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+// import 뭔가요? 몰라도 됨
+
+const WinePage: NextPage = () => {
+  return (
+    <div>
+      Wine
+    </div>
+  )
+}
+
+export default WinePage;
+// export 뭔가요? > 아직 몰라도됨
