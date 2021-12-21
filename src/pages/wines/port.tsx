4e388123c57f3923c6c61b7474fe69ec825b025f
seungyeon-lee3214 <54584337+seungyeon-lee3214@users.noreply.github.com>
2021-12-22 06:21:19 +0900

    컴포넌트 리팩토링

--- src/pages/wines/port.tsx
+++ src/pages/wines/port.tsx
@@ -1,7 +1,33 @@
 import type { NextPage } from 'next';
+import { Error, Loading, WineCard } from '../../components'
+import { useWineData } from '../../hooks/useWineData';
+import { Wine } from '../../types/Wine';
+
+
+
 
 const PortPage: NextPage = () => {
-    return <div>Port Wine</div>;
+    const name = 'port';
+    const { data, error } = useWineData(name)
+
+    if (error) return <Error />
+    if (!data) return <Loading />
+
+    return (
+        <div>
+            <h1>Wine</h1>
+            <main>
+                {data.map((wineData: Wine) => {
+                    return (
+                        <WineCard
+                         key={`port-wine-list-${wineData.id}`}
+                            wineData={wineData}
+                            />
+                    )
+                })}
+            </main>
+        </div>
+    );
 };
 
 export default PortPage;
